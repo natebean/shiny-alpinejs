489b95677187c0f13bfb57fce9b81cfc75b5f7a3
Nathan Bean <natejbean@gmail.com>
2021-10-27 20:45:20 -0500

    Shiny input and output.

--- app.R
+++ app.R
@@ -1,27 +1,27 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
+
 
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-  tags$head(
-    tags$script(src = "https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js", defer = NA),
-    tags$script(src = "index.js"),
-  ),
+  # tags$head(
+  #   tags$script(src = "https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js", defer = NA),
+  #   tags$script(src = "index.js"),
+  # ),
   h1("Hello World"),
   p(`x-data` = "index", `x-text` = "value"),
-  p(`x-data` = "indexA", `x-text` = "value"),
-  includeHTML("www/include.html")
+  includeHTML("www/include.html"),
+  textOutput("first_round_trip")
 )
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+
+server <- function(input, output, session) {
+  session$sendCustomMessage("first-message-from-shiny", "Hi from Shiny")
+  print('running')
+  
+  output$first_round_trip <- renderText({
+    print('first_input')
+    paste(input$first_input, " and back")
+  })
   
 }
 
--- www/index.js
+++ www/index.js
@@ -1,9 +1,36 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('index', () => ({
-      value: 'index Data Object',
-      includeValue:  'Alpine + Shiny + Include'
-    }))
-    Alpine.data('indexA', () => ({
-      value: 'indexA Data Object'
-    }))
-})
+function sendMessage(message) {
+  console.log("Button Pressed");
+  console.log("message", message);
+  if (typeof Shiny !== "undefined") {
+    Shiny.setInputValue("first_input", "Hi from JS");
+  }
+}
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("index", () => ({
+    value: "index Data Object",
+    includeValue: "Alpine + Shiny + Include",
+  }));
+
+  Alpine.data("tableTest", () => ({
+    data: [
+      { one: "one1", two: "two1" },
+      { one: "one2", two: "two2" },
+    ],
+  }));
+
+  if (typeof Shiny !== "undefined") {
+    Shiny.addCustomMessageHandler(
+      "first-message-from-shiny",
+      function (message) {
+        console.log(message);
+        let event = new CustomEvent("first-message", {
+          detail: {
+            message: message,
+          },
+        });
+        window.dispatchEvent(event);
+      }
+    );
+  }
+});
