8b38ad34fd020a0e10b604057794e5da03fd1c97
Nate Bean <natejbean@gmail.com>
2021-11-08 12:22:50 -0600

    Better edit tools panel.  CSS in file

--- app.R
+++ app.R
@@ -9,8 +9,14 @@ ui <- fluidPage(
   tags$head(
     tags$script(src = "alpine-loader.js"),
     tags$script(src = "index.js"),
-    tags$link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css"),
-    includeCSS("www/table-example.css")
+    tags$link(
+      rel = "stylesheet",
+      href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css"
+    ),
+    tags$link(
+      rel = "stylesheet",
+      href = "table-example.css"
+    )
   ),
   div(
     class = "container",
@@ -18,62 +24,79 @@ ui <- fluidPage(
     div(
       `x-shiny-data` = "tableData",
       class = "container",
-      div(style = "margin: 10px 0",
-          tags$button(`@click` = "updateTableData", "Refresh")),
-      tags$table(class = "table table-border",
-                 tags$thead(tags$tr(
-                   tags$th(class = "text-column", "Column"),
-                   tags$th(class = "text-column", "Title"),
-                   tags$th(class = "robot-column", tags$i(class = "bi bi-robot"))
-                 )),
-                 tags$tbody(
-                   tags$template(`x-for` = "row in data",
-                                 tags$tr(
-                                   tags$td(`x-text` = "row.column"),
-                                   tags$td(
-                                     tags$div(
-                                       class = "title-value",
-                                       `x-data` = "{open: false, editing: false}",
-                                       `@mouseover` = "open = true",
-                                       `@mouseout` = "open = false",
-                                       tags$span(`x-text` = "row.title",
-                                                 `x-show` = "!editing"),
-                                       tags$div(
-                                         `x-show` = "open && !editing",
-                                         class = "edit-panel",
-                                         tags$i(class = "bi bi-pencil edit-icon",  `@click` = "editing = true;"),
-                                         tags$i(class = "bi bi-check-circle edit-icon",
-                                                `@click` = "row.robot = 'green-robot'"),
-                                         tags$i(class = "bi bi-x-circle edit-icon",
-                                                `@click` = "row.robot = 'hidden-robot'")
-                                         
-                                       ),
-                                       tags$div(
-                                         tags$input(
-                                           type = "text",
-                                           `x-show` = "editing",
-                                           `:value` = "row.title",
-                                           `@keyup.enter` = "editing = false; row.title = $event.target.value.trim(); $event.target.value = row.title;",
-                                           `@blur` = "editing = false"
-                                         )
-                                       )
-                                       
-                                     )
-                                   ),
-                                   tags$td(class = "robot-column",
-                                           tags$i(class = "bi bi-robot", `:class` = "row.robot"))
-                                 ))
-                 )),
-      tags$button(`@click` = "sendDataToShiny('tableData')", "Save")
+      div(
+        style = "margin: 10px 0",
+        tags$button(`@click` = "updateTableData", class = "btn btn-dark", "Refresh")
+      ),
+      tags$table(
+        class = "table table-border narrow-table",
+        tags$thead(tags$tr(
+          tags$th(class = "text-column", "Column"),
+          tags$th(class = "text-column", "Title"),
+          tags$th(class = "robot-column", tags$i(class = "bi bi-robot"))
+        )),
+        tags$tbody(
+          tags$template(
+            `x-for` = "row in data",
+            tags$tr(
+              tags$td(`x-text` = "row.column"),
+              tags$td(
+                tags$div(
+                  class = "title-value",
+                  `x-data` = "{open: false, editing: false}",
+                  `@mouseover` = "open = true",
+                  `@mouseout` = "open = false",
+                  tags$span(
+                    `x-text` = "row.title",
+                    `x-show` = "!editing"
+                  ),
+                  tags$div(
+                    `x-show` = "open && !editing",
+                    class = "edit-panel",
+                    tags$i(class = "bi bi-pencil edit-icon", `@click` = "editing = true;"),
+                    tags$i(
+                      class = "bi bi-check-circle edit-icon",
+                      style = "color: green",
+                      `@click` = "row.robot = 'green-robot'"
+                    ),
+                    tags$i(
+                      class = "bi bi-x-circle edit-icon",
+                      style = "color: red",
+                      `@click` = "row.robot = 'hidden-robot'"
+                    )
+                  ),
+                  tags$div(
+                    tags$input(
+                      type = "text",
+                      `x-show` = "editing",
+                      `:value` = "row.title",
+                      `@keyup.enter` = "editing = false; row.title = $event.target.value.trim(); $event.target.value = row.title;",
+                      `@blur` = "editing = false"
+                    )
+                  )
+                )
+              ),
+              tags$td(
+                class = "robot-column",
+                tags$i(class = "bi bi-robot", `:class` = "row.robot")
+              )
+            )
+          )
+        )
+      ),
+      tags$button(
+        `@click` = "sendDataToShiny('tableData')",
+        class = "btn btn-dark", "Save"
+      )
     )
   )
 )
 
 server <- function(input, output, session) {
   table_data <- tribble(
-    ~ column,
-    ~ title,
-    ~ robot,
+    ~column,
+    ~title,
+    ~robot,
     "one-column",
     "one-title",
     "green-robot",
@@ -81,10 +104,9 @@ server <- function(input, output, session) {
     "two-title",
     "red-robot"
   )
-  
-  
+
   update_alpine_data(session, "tableData", table_data)
-  
+
   observeEvent(input$tableData_data, {
     ans <- convert_from_alpine(input$tableData_data)
     print(ans)
--- shiny-alpinejs.R
+++ shiny-alpinejs.R
@@ -1,27 +1,17 @@
 library(jsonlite)
-library(htmltools)
 
 initialize_alpine <- function(session, data) {
-  session$sendCustomMessage("alpine-initalize", toJSON(data))
+  session$sendCustomMessage("alpine-initalize", jsonlite::toJSON(data))
 }
 
 update_alpine_data <- function(session, name, data) {
   data_package <- list(name = name, data = data)
-  session$sendCustomMessage("alpine-update-data", toJSON(data_package))
+  session$sendCustomMessage(
+    "alpine-update-data",
+    jsonlite::toJSON(data_package)
+  )
 }
 
 convert_from_alpine <- function(data) {
-  fromJSON(data)
-}
-
-alpine_template <- function(...,
-                            .noWS = NULL,
-                            .renderHook = NULL)
-{
-  # validateNoWS(.noWS)
-  contents <- list(...)
-  tag("template",
-      contents,
-      .noWS = .noWS,
-      .renderHook = .renderHook)
+  jsonlite::fromJSON(data)
 }
